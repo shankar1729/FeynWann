c142cabc7a8550e5d8c0c9c1340376c6bcf50d88
Ana Brown <anabrown8@gmail.com>
2014-11-20 23:07:29 +0000

    changing implementation for dFdE, now kappa ~9....

--- plasmonPhononDecay.cpp
+++ plasmonPhononDecay.cpp
@@ -106,7 +106,7 @@ int main(int argc, char** argv)
 			N1block += exp(-0.5*mk1k2/(T*T));
 			diagMatrix Ek = bs.getStates(kpnt1);
 			for(int n = 0; n<Ek.nRows(); n++)
-			{	double dFdE =(-1/T)*exp((Ek[n])/T)/std::pow((exp((Ek[n])/T)+1),2);
+			{	double dFdE =-1/(T*std::pow(2*cosh(Ek[n]/(2*T)),2));
 				kappaSqrdBlock += fabs(4*M_PI*spinWeight*dFdE);
 			}
 		}
