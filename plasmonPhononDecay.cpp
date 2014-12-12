bd1b8ef2e2cd9cec99ba70217d1977f570228bbf
Ana Brown <anabrown8@gmail.com>
2014-12-12 19:46:14 +0000

    continuing work on gamma calcualtion

--- plasmonPhononDecay.cpp
+++ plasmonPhononDecay.cpp
@@ -180,19 +180,22 @@ int main(int argc, char** argv)
 					std::vector<matrix> Pk1 = bs.getDipoleMatElem(kpnt1);
 					diagMatrix E2 = bs.getStates(kpnt2);
 					std::vector<matrix> Pk2 = bs.getDipoleMatElem(kpnt2);
+					diagMatrix P = getPhononModes(kpnt1-kpnt2);
 					std::vector<matrix> PePh = bs.getPhononMatElem(kpnt1,kpnt2);
 					for(int v=0; v<E1.nRows(); v++) if(E1[v]<10.*T)
 					{	for(int c=0; c<E2.nRows(); c++) if(E2[c]>-10.*T)
 						{	for(int alpha = 0; alpha < PePh.size(); alpha ++)
-							{	double mk_cv = BandStruct::mk_sub(E2[c], E1[v], Eplasmon, T);
-								double weightEconserve = exp(0.5*(mk1k2-mk_cv)/(T*T))/(T*sqrt(2*M_PI)); //weight contribution due to energy conservation
-								if(weightEconserve < weightCut) continue;
-								// Effective matrix elements
-								complex prefacDotP = 0.;
-								for(int i=0; i<E1.nRows(); i++) // sum over the intermediate states
-								{	complex E2i(E2[i], lineWidth(E2[i])), E1i(E1[i], lineWidth(E1[i]));
-									if(E2[i] < energyCut && E1[i] < energyCut) for(int j=0; j<3; j++) 
-										prefacDotP += sqrtGammaPrefac[j] * (Pk2[j](c,i)*(1-1/(exp(E2[i]/T)+1))*PePh[alpha](c,i)/(E2i-E2[c]+Eplasmon) + sqrtGammaPrefac[j] * (Pk2[j](i,v)*(1-1/(exp(E1[i]/T)+1))*PePh[alpha](i,v)/(E1i-E1[v]-Eplasmon);
+							{	for(int ae = -1; ae <=1; ae+=2) // +/- for phonon absorption or emmision
+								{	double mk_cv = BandStruct::mk_sub(E2[c], E1[v], Eplasmon+ae*P(c,v), T);
+									double weightEconserve = exp(0.5*(mk1k2-mk_cv)/(T*T))/(T*sqrt(2*M_PI)); //weight contribution due to energy conservation
+									if(weightEconserve < weightCut) continue;
+									// Effective matrix elements
+									complex prefacDotP = 0.;
+									for(int i=0; i<E1.nRows(); i++) // sum over the intermediate states
+									{	complex E2i(E2[i], lineWidth(E2[i])), E1i(E1[i], lineWidth(E1[i]));
+										if(E2[i] < energyCut && E1[i] < energyCut) for(int j=0; j<3; j++) 
+											prefacDotP += sqrtGammaPrefac[j] * (Pk2[j](c,i)*(1-1/(exp(E2[i]/T)+1))*PePh[alpha](c,i)/(E2i-E2[c]+Eplasmon) + sqrtGammaPrefac[j] * (Pk2[j](i,v)*(1-1/(exp(E1[i]/T)+1))*PePh[alpha](i,v)/(E1i-E1[v]-Eplasmon);
+									}
 								}
 							}
 							double weight = (0.5*spinWeight) * weightEconserve * prefacDotP.norm(); //norm = abs^2
