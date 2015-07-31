8e529e25eb7f4677dd31f1a54abb556fdc9508bb
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2015-07-31 17:46:07 +0000

    Updating surface-assisted expression in drudeGamma

--- drudeGamma.cpp
+++ drudeGamma.cpp
@@ -44,21 +44,27 @@ int main(int argc, char** argv)
 	//Jellium parameters:
 	double nJellium = Zjellium / fabs(det(R));
 	double omegaPsq = 4*M_PI * nJellium; logPrintf("Jellium plasma frequency: %lg eV\n", sqrt(omegaPsq)/eV);
-	double vF = std::pow(3*M_PI*M_PI*nJellium, 1./3); //same as kF in atomic units
+	double kF = std::pow(3*M_PI*M_PI*nJellium, 1./3);
+	double vF = kF; //same as kF in atomic units
 	
 	//Initialize dielectric model:
 	Epsilon eps("Wannier/epsilon.dat");
 	complex epsilon;
 
 	ofstream ofs1("drudeEpsilon.dat");
-	ofstream ofs2("drudeGamma.dat");	
+	ofstream ofs2("drudeGamma.dat");
 
 	double gaussMargin = 5*T;
 	for(double omega = gaussMargin; omega <= EplasmonMax-gaussMargin; omega += T)
 	{	eps.setFrequency(omega, false);
 		complex sigma = complex(sigma_o) / complex(1,-omega*tau);
 		epsilon = 1.+ 4*M_PI*complex(-imag(sigma),real(sigma)) / complex(omega);
-		double imEpsSurface = 0.75 * omegaPsq * eps.modGammaMinus * vF / std::pow(omega,3);
+		double imEpsSurface = 0.75 * omegaPsq * eps.modGammaMinus * vF / std::pow(omega,3) //Khurgin's version
+			* 2./(1.+std::pow(eps.modGammaMinus/eps.k, 2)) //Our main correction
+			* ( 1. //+ higher-order corrections:
+				- (1./4)*std::pow(omega/(0.5*kF*kF),2)*log(2*kF*kF/omega) //high-frequency correction
+				+ (1./6)*std::pow(eps.modGammaMinus/eps.k,2)/(1.+std::pow(omega/(vF*eps.modGammaMinus),2)) //low-frequency correction
+			);
 		double prefac = eps.exptLinewidth()/eps.epsilon.imag(); //ratio between plasmon linewidth and imaginary part of epsilon
 		ofs1 << omega/eV << '\t' << real(epsilon) << '\t' << imag(epsilon) << '\n'; //Output energies in eV units
 		ofs2 << omega/eV << '\t' << prefac*imag(epsilon)/eV << '\t' << prefac*imEpsSurface/eV << '\n'; //Output energies in eV units
