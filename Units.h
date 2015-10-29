571dcf302fe1d02e4d47f177df2e20662c4be4a2
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2015-10-29 22:22:09 +0000

    Switches to make WannierMetropolis compatible with most recent JDFTx

--- Units.h
+++ Units.h
@@ -8,6 +8,5 @@ const double Coulomb = Joule/eV;
 const double Volt = Joule/Coulomb;
 const double Ampere = Coulomb*invSeconds;
 const double Ohm = Volt/Ampere;
-const double fs = 1e-15/invSeconds;
 
 #endif //WANNIERMETROPOLIS_UNITS_H
