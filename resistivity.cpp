6be2ed164acbb0f93f23d2ea8ca0bd5e149da60f
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2016-11-12 22:26:53 +0000

    Added reliable handling of 2D resistivity

--- resistivity.cpp
+++ resistivity.cpp
@@ -42,6 +42,25 @@ void reportResult(const std::vector<double>& result, string resultName, double u
 	logPrintf("%17s = %12lg +/- %12lg %s\n", resultName.c_str(), resultMean/unit, resultStd/unit, unitName.c_str());
 }
 
+//Eliminate direction slabDir from tensor (for 2D case normal to slabDir):
+inline void slabConstrain(matrix3<>& M, int slabDir)
+{	if(slabDir >= 0)
+	{	for(int jDir=0; jDir<3; jDir++)
+		{	M(jDir, slabDir) = 0.;
+			M(slabDir, jDir) = 0.;
+		}
+		M(slabDir, slabDir) = 1.;
+	}
+}
+
+//Trace directions other than slabDir:
+inline double trace(const matrix3<>& M, int slabDir)
+{	double result = 0;
+	for(int jDir=0; jDir<3; jDir++)
+		if(jDir != slabDir)
+			result += M(jDir,jDir);
+	return result;
+}
 
 int main(int argc, char** argv)
 {	string inputFilename; bool dryRun, printDefaults;
@@ -56,6 +75,7 @@ int main(int argc, char** argv)
 	const double dmuMin = inputMap.get("dmuMin", 0.) * eV; //optional shift in chemical potential from neutral value; start of range (default to 0)
 	const double dmuMax = inputMap.get("dmuMax", 0.) * eV; //optional shift in chemical potential from neutral value; end of range (default to 0)
 	const int dmuCount = inputMap.get("dmuCount", 1); assert(dmuCount>0); //number of chemical potential shifts
+	const int slabDir = inputMap.get("slabDir", -1); assert(slabDir<3); //0-based index of direction to eliminate; default -1 => don't eliminate any (keep 3D)
 	
 	logPrintf("\nInputs after conversion to atomic units:\n");
 	logPrintf("nKpts = %d\n", nKpts);
@@ -65,6 +85,7 @@ int main(int argc, char** argv)
 	logPrintf("dmuMin = %lg\n", dmuMin);
 	logPrintf("dmuMax = %lg\n", dmuMax);
 	logPrintf("dmuCount = %d\n", dmuCount);
+	logPrintf("slabDir = %d\n", slabDir);
 	
 	//Initialize Wannier bandstructure:
 	const int bunchSize = 32;
@@ -108,6 +129,18 @@ int main(int argc, char** argv)
 	for(int iMu=1; iMu<dmuCount; iMu++) //set remaining values (if any)
 		dmu[iMu] = dmuMin + iMu*(dmuMax-dmuMin)/(dmuCount-1);
 	
+	//Handle dimensionality:
+	double Omega = fabs(det(bs.R));
+	double rhoUnit = 1e-9*Ohm*meter;
+	string rhoUnitName="nOhm-m";
+	string rhoName = "Resistivity";
+	if(slabDir>=0)
+	{	Omega /= bs.R.column(slabDir).length(); //convert to area excluding this dimension
+		rhoUnit = Ohm;
+		rhoUnitName = "Ohm";
+		rhoName = "SheetResistance";
+	}
+	
 	//Compute resistivity:
 	#define DeclareArray2D(type, name) std::vector<std::vector<type>> name(dmuCount, std::vector<type>(nBlocks))
 	DeclareArray2D(matrix3<>, Tarr); DeclareArray2D(matrix3<>, GammaArr); DeclareArray2D(matrix3<>, rhoArr);
@@ -115,7 +148,6 @@ int main(int argc, char** argv)
 	DeclareArray2D(double, vFarr); DeclareArray2D(double, gArr); 
 	#undef DeclareArray2D
 	int nKptsMin = ceildiv(nKpts, nBlocks*mpiUtil->nProcesses()); //number of k points per block per process
-	double Omega = fabs(det(bs.R));
 	const double Emax = std::max(fabs(dmuMin), fabs(dmuMax)) + 10*T + 5*EconserveWidth; //max energy from Fermi level to consider
 	double EconserveExpFac = -0.5/std::pow(EconserveWidth,2), EconservePrefac = 1./(sqrt(2*M_PI)*EconserveWidth); //energy conserving Gaussian parameters
 	for(int block=0; block<nBlocks; block++)
@@ -217,15 +249,23 @@ int main(int argc, char** argv)
 			g[iMu] *= prefacT;
 			Gamma[iMu] *= prefacGamma; Gamma[iMu] = 0.5*(Gamma[iMu] + (~Gamma[iMu])); //symmetrize
 			tauInv[iMu] *= prefacGamma;
+			slabConstrain(Tcur[iMu], slabDir); //eliminate out-of-plane components if necessary
+			slabConstrain(Gamma[iMu], slabDir); //eliminate out-of-plane components if necessary
 			//Store relevant quantities:
 			Tarr[iMu][block] = Tcur[iMu];
 			GammaArr[iMu][block] = Gamma[iMu];
 			rhoArr[iMu][block] = Omega * (inv(Tcur[iMu]) * Gamma[iMu] * inv(Tcur[iMu]));
-			rhoBarArr[iMu][block] = trace(rhoArr[iMu][block])/3.;
+			rhoBarArr[iMu][block] = trace(rhoArr[iMu][block], slabDir) / (slabDir>=0 ? 2. : 3.);
 			tauArr[iMu][block] = g[iMu] / tauInv[iMu];
-			tauDrudeArr[iMu][block] = trace(Tcur[iMu]) / trace(Gamma[iMu]);
-			vFarr[iMu][block] = sqrt(trace(Tcur[iMu])/g[iMu]);
+			tauDrudeArr[iMu][block] = trace(Tcur[iMu], slabDir) / trace(Gamma[iMu], slabDir);
+			vFarr[iMu][block] = sqrt(trace(Tcur[iMu], slabDir)/g[iMu]);
 			gArr[iMu][block] = g[iMu];
+			//Fix slab direction values physically:
+			if(slabDir>=0.)
+			{	Tarr[iMu][block](slabDir,slabDir) = 0.;
+				GammaArr[iMu][block](slabDir,slabDir) = 0.;
+				rhoArr[iMu][block](slabDir,slabDir) = INFINITY;
+			}
 		}
 	}
 	logPrintf("Done.\n\n");
@@ -234,8 +274,8 @@ int main(int argc, char** argv)
 	{	logPrintf("\nResults for dmu = %lg eV:\n", dmu[iMu]/eV);
 		reportResult(Tarr[iMu], "T", 1, "");
 		reportResult(GammaArr[iMu], "Gamma", 1, "");
-		reportResult(rhoArr[iMu], "Resistivity", 1e-9*Ohm*meter, "nOhm-m");
-		reportResult(rhoBarArr[iMu], "Resistivity", 1e-9*Ohm*meter, "nOhm-m");
+		reportResult(rhoArr[iMu], rhoName, rhoUnit, rhoUnitName);
+		reportResult(rhoBarArr[iMu], rhoName, rhoUnit, rhoUnitName);
 		reportResult(tauDrudeArr[iMu], "tauDrude", fs, "fs");
 		reportResult(tauArr[iMu], "tau", fs, "fs");
 		reportResult(vFarr[iMu], "vF", 1, "");
