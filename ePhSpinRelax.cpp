f49b9b2c47c9e17f07e591c623de2fbcc6c834b9
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2020-03-26 17:46:51 -0400

    Merged dRho formula into ePhSpinRelax which now outputs both FGR and dRho results for cross validation

--- ePhSpinRelax.cpp
+++ ePhSpinRelax.cpp
@@ -31,10 +31,11 @@ struct SpinRelaxCollect
 	const double omegaPhByTmin; //lower cutoff in phonon frequency (relative to T)
 	const int nModes; //number of phonon modes to include in calculation (override if any, applied below already)
 	
-	const double EconserveExpFac; //exponential factor in Gaussian delta for energy conservation
+	const double degeneracyThreshold; //threshold for degenerate subspace projection
+	const double sqrtEconserveExpFac; //exponential factor in sqrt of Gaussian delta for energy conservation
 	const double prefacGamma, prefacChi; //prefactors for numerator and denominator of T1
 	const double Estart, Estop; //energy range close enough to band edges or mu's to be relevant
-	std::vector<matrix3<>> Gamma, chi; //numerator and denominator in T1^-1, for each T and dmu 
+	std::vector<matrix3<>> Gamma, chi; //numerator and denominator in T1^-1, for each T, dmu and formula (FGR vs dRho)
 	
 	//Quantities for evaluating inter-valley contributions
 	const bool valley; //whether to evaluate inter-valley contributions
@@ -42,21 +43,20 @@ struct SpinRelaxCollect
 	const vector3<> K, Kp;
 	std::vector<matrix3<>> GammaV; //valley-weighted numerator of T1^-1
 	
-	SpinRelaxCollect(const std::vector<double>& dmu, const std::vector<double>& T, double omegaPhByTmin, int nModes,
+	SpinRelaxCollect(const std::vector<double>& dmu, const std::vector<double>& T, double omegaPhByTmin, int nModes, double degeneracyThreshold,
 		double EconserveWidth, size_t nKpairs, size_t nKtot, double Estart, double Estop, bool valley, matrix3<> R)
-	: dmu(dmu), T(T), omegaPhByTmin(std::max(1e-3,omegaPhByTmin)), nModes(nModes),
-		EconserveExpFac(-0.5/std::pow(EconserveWidth, 2)),
+	: dmu(dmu), T(T), omegaPhByTmin(std::max(1e-3,omegaPhByTmin)), nModes(nModes), degeneracyThreshold(degeneracyThreshold),
+		sqrtEconserveExpFac(-0.25/std::pow(EconserveWidth, 2)),
 		prefacGamma(2*M_PI/ (nKpairs * sqrt(2.*M_PI)*EconserveWidth)), //include prefactor of Gaussian energy conservation
 		prefacChi(0.5/nKtot), //collected over both k1 and k2 arrays
 		Estart(Estart), Estop(Estop),
-		Gamma(T.size()*dmu.size()), chi(T.size()*dmu.size()),
+		Gamma(T.size()*dmu.size()*2), chi(T.size()*dmu.size()*2),
 		valley(valley), G(2*M_PI * inv(R)), GGT(G * (~G)), K(1./3, 1./3, 0), Kp(-1./3, -1./3, 0), GammaV(Gamma)
 	{
 	}
 	
 	inline SparseMatrix degenerateProject(const matrix& M, const diagMatrix& E, int bStart, int bStop)
-	{	static const double degeneracyThreshold = 1e-6;
-		SparseMatrix result; result.reserve(bStop-bStart); //typically diagonal (Rashba) or block diagonal with size 2 (Kramer-degenerate)
+	{	SparseMatrix result; result.reserve(bStop-bStart); //typically diagonal (Rashba) or block diagonal with size 2 (Kramer-degenerate)
 		for(int b2=bStart; b2<bStop; b2++)
 		{	const complex* Mdata = M.data() + (b2*M.nRows() + bStart);
 			for(int b1=bStart; b1<bStop; b1++)
@@ -85,6 +85,34 @@ struct SpinRelaxCollect
 			< GGT.metric_length_squared(wrap(Kp-k));
 	}
 	
+	//Narrow band range based on active energy range:
+	void bandRangeNarrow(const FeynWann::StateE& e, int& bStart, int & bStop)
+	{	for(int b=0; b<e.E.nRows(); b++)
+		{	const double& E = e.E[b];
+			if(E>=Estart and b<bStart) bStart=b;
+			if(E<=Estop and b>=bStop) bStop=b+1;
+		}
+	}
+	
+	//Change in density matrix due to first order perturbation theory by Hamiltonian H1:
+	inline matrix dRhoCalc(const diagMatrix& E, const diagMatrix& F, double T, const matrix& H1, int bStart, int bStop)
+	{	int nBandsSel = bStop - bStart;
+		matrix result(nBandsSel, nBandsSel); complex *rData = result.data();
+		double invT = 1./T;
+		for(int b2 = bStart; b2 < bStop; b2++)
+			for(int b1 = bStart; b1 < bStop; b1++)
+			{	double minus_fPrimeEff = 0.;
+				if(fabs(E[b1] - E[b2]) <= degeneracyThreshold)
+				{	double Favg = 0.5 * (F[b1] + F[b2]);
+					minus_fPrimeEff =  Favg * (1. - Favg) * invT; //-df/dE
+				}
+				else minus_fPrimeEff = (F[b1] - F[b2]) / (E[b2] - E[b1]); //finite difference version of -df/dE
+				*(rData++) = minus_fPrimeEff * H1(b1, b2);
+			}
+		return result;
+	}
+
+	
 	//Calculate spin relaxation rate numerator (Gamma):
 	void process(const FeynWann::MatrixEph& mEph)
 	{	const FeynWann::StateE& e1 = *(mEph.e1);
@@ -93,33 +121,35 @@ struct SpinRelaxCollect
 		const int nBands = e1.E.nRows();
 		
 		//Select relevant band range:
-		int bStart = nBands, bStop = 0;
-		#define SET_bRange(s) \
-			for(int b=0; b<nBands; b++) \
-			{	const double& E = e##s.E[b]; \
-				if(E>=Estart and b<bStart) bStart=b; \
-				if(E<=Estop and b>=bStop) bStop=b+1; \
-			}
-		SET_bRange(1)
-		SET_bRange(2)
-		#undef SET_bRange
-		int nBandsSel = bStop - bStart; //reduced number of selected bands at this k-pair
-		int nBandsSelSq = nBandsSel * nBandsSel;
-		if(nBandsSel <= 0) return;
+		int bStart1 = nBands, bStop1 = 0;
+		int bStart2 = nBands, bStop2 = 0;
+		bandRangeNarrow(e1, bStart1, bStop1);
+		bandRangeNarrow(e2, bStart2, bStop2);
+		int nBandsSel1 = bStop1 - bStart1; //reduced number of selected bands at this k-pair
+		int nBandsSel2 = bStop2 - bStart2; //reduced number of selected bands at this k-pair
+		int nBandsSelProd = nBandsSel1 * nBandsSel2;
+		if(nBandsSelProd <= 0) return;
 		
 		//Degenerate spin projections:
 		std::vector<SparseMatrix> Sdeg1(3), Sdeg2(3);
+		std::vector<matrix> S1(3), S2(3);
 		for(int iDir=0; iDir<3; iDir++)
-		{	Sdeg1[iDir] = degenerateProject(e1.S[iDir], e1.E, bStart, bStop);
-			Sdeg2[iDir] = degenerateProject(e2.S[iDir], e2.E, bStart, bStop);
+		{	Sdeg1[iDir] = degenerateProject(e1.S[iDir], e1.E, bStart1, bStop1);
+			Sdeg2[iDir] = degenerateProject(e2.S[iDir], e2.E, bStart2, bStop2);
+			S1[iDir] = e1.S[iDir](bStart1,bStop1, bStart1,bStop1);
+			S2[iDir] = e2.S[iDir](bStart2,bStop2, bStart2,bStop2);
 		}
 		
 		//Weight for valley contrib
 		double wValley = (valley and (isKvalley(e1.k) xor isKvalley(e2.k))) ? 1. : 0.;
 		
 		//Compute Gamma contributions by band pair and T, except for electron occupation factors:
-		std::vector<std::vector<matrix3<>>> contribGamma(T.size(), std::vector<matrix3<>>(nBandsSelSq));
+		std::vector<std::vector<matrix3<>>> contribGamma(T.size(), std::vector<matrix3<>>(nBandsSelProd));
 		std::vector<std::vector<matrix3<>>> contribGammaV(contribGamma); //for valley contributions
+		std::vector<matrix> A(nModes); //phonon matrix elements * sqrt(energy conservation) for each phonon mode
+		std::vector<vector3<matrix>> SAcomm(nModes); //commutator of A with spin matrices
+		std::vector<diagMatrix> nPh(T.size(), diagMatrix(nModes)); //phonon occupation factors at each temperature
+		std::vector<bool> modeContrib(nModes, false); //whether any Econserve factor non-zero for given phonon mode
 		for(int alpha=0; alpha<nModes; alpha++)
 		{	//Phonon occupation (nPh/T and prefactors) for each T:
 			const double& omegaPh = ph.omega[alpha];
@@ -128,31 +158,38 @@ struct SpinRelaxCollect
 			{	double invT = 1./T[iT];
 				const double omegaPhByT = invT*omegaPh;
 				if(omegaPhByT < omegaPhByTmin) continue; //avoid 0./0. below
-				prefac_nPhByT[iT] =  prefacGamma * invT * bose(omegaPhByT);
+				nPh[iT][alpha] = bose(omegaPhByT);
+				prefac_nPhByT[iT] =  prefacGamma * invT * nPh[iT][alpha];
 			}
 			//Energy conservation factor by band pairs:
-			std::vector<double> Econserve(nBandsSelSq);
+			const matrix G = mEph.M[alpha](bStart1,bStop1, bStart2,bStop2); //work only with sub-matrix of relevant bands
+			A[alpha] = G; //G multiplied by sqrt(Econserve) below
+			std::vector<double> Econserve(nBandsSelProd);
 			int bIndex = 0;
 			bool contrib = false;
-			for(int b2=bStart; b2<bStop; b2++)
-			for(int b1=bStart; b1<bStop; b1++)
-			{	double expTerm = EconserveExpFac * std::pow(e1.E[b1] - e2.E[b2] - omegaPh, 2);
-				if(expTerm > -15.)
-				{	Econserve[bIndex] = exp(expTerm); //compute exponential only when needed
+			complex* Adata = A[alpha].data();
+			for(int b2=bStart2; b2<bStop2; b2++)
+			for(int b1=bStart1; b1<bStop1; b1++)
+			{	double expTerm = sqrtEconserveExpFac * std::pow(e1.E[b1] - e2.E[b2] - omegaPh, 2);
+				if(expTerm > -8)
+				{	double sqrtEconserve = exp(expTerm); //compute exponential only when needed
+					Adata[bIndex] *= sqrtEconserve;
+					Econserve[bIndex] = sqrtEconserve * sqrtEconserve;
+					modeContrib[alpha] = true;
 					contrib = true;
 				}
 				bIndex++;
 			}
 			if(not contrib) continue; //no energy conserving combination for this phonon mode at present k-pair
-			//Calculate commutators:
-			const matrix G = mEph.M[alpha](bStart,bStop, bStart,bStop); //work only with sub-matrix of relevant bands
+			//Calculate spin-phonon commutators:
 			matrix SGcomm[3]; vector3<const complex*> SGcommData;
 			for(int iDir=0; iDir<3; iDir++)
-			{	SGcomm[iDir] = Sdeg1[iDir] * G - G * Sdeg2[iDir];
+			{	SGcomm[iDir] = Sdeg1[iDir] * G - G * Sdeg2[iDir]; //for FGR version
 				SGcommData[iDir] = SGcomm[iDir].data();
+				SAcomm[alpha][iDir] = S1[iDir] * A[alpha] - A[alpha] * S2[iDir]; //for dRho version
 			}
-			//Collect commutator contributions:
-			for(int bIndex=0; bIndex<nBandsSelSq; bIndex++) //loop over b2 and b1
+			//Collect commutator contributions for each T (FGR version):
+			for(int bIndex=0; bIndex<nBandsSelProd; bIndex++) //loop over b2 and b1
 			{	vector3<complex> SGcommCur = loadVector(SGcommData, bIndex);
 				matrix3<> SGcommOuter = realOuter(SGcommCur, SGcommCur);
 				for(size_t iT=0; iT<T.size(); iT++)
@@ -168,25 +205,45 @@ struct SpinRelaxCollect
 		for(size_t iT=0; iT<T.size(); iT++)
 		{	double invT = 1./T[iT];
 			for(size_t iMu=0; iMu<dmu.size(); iMu++)
-			{	size_t iMuT = iT*dmu.size() + iMu; //combined index
-				//Compute Fermi occupations:
-				#define CALC_F(s) \
+			{	size_t iMuT = (iT*dmu.size() + iMu)*2; //combined index
+				//Compute Fermi occupations and dRho perturbations:
+				#define CALC_F_dRho(s) \
 					diagMatrix F##s(nBands), Fbar##s(nBands); \
-					for(int b=bStart; b<bStop; b++) \
-						fermi(invT*(e##s.E[b] - dmu[iMu]), F##s[b], Fbar##s[b]);
-				CALC_F(1)
-				CALC_F(2)
-				#undef CALC_F
+					vector3<matrix> dRho##s; \
+					for(int b=bStart##s; b<bStop##s; b++) \
+					{	fermi(invT*(e##s.E[b] - dmu[iMu]), F##s[b], Fbar##s[b]); \
+					} \
+					for(int jDir=0; jDir<3; jDir++) \
+						dRho##s[jDir] = dRhoCalc(e##s.E, F##s, T[iT], e##s.S[jDir], bStart##s, bStop##s);
+				CALC_F_dRho(1)
+				CALC_F_dRho(2)
+				#undef CALC_F_dRho
 				
-				//Accumulate Gamma contributions:
+				//Accumulate Gamma contributions (FGR version):
 				int bIndex = 0;
-				for(int b2=bStart; b2<bStop; b2++)
-				for(int b1=bStart; b1<bStop; b1++)
+				for(int b2=bStart2; b2<bStop2; b2++)
+				for(int b1=bStart1; b1<bStop1; b1++)
 				{	Gamma[iMuT] += contribGamma[iT][bIndex] * (F2[b2] * Fbar1[b1]);
 					if(valley) //inter-valley weighted result
 						GammaV[iMuT] += contribGammaV[iT][bIndex] * (F2[b2] * Fbar1[b1]);
 					bIndex++;
 				}
+				
+				//Accumulate Gamma contributions (dRho version):
+				for(int alpha=0; alpha<nModes; alpha++)
+					if(modeContrib[alpha])
+					{	matrix3<> contribGamma_dRho;
+						diagMatrix nPhF1(bStop1-bStart1, nPh[iT][alpha]); nPhF1 += F1(bStart1,bStop1); //nPh + f1
+						diagMatrix nPhFbar2(bStop2-bStart2, nPh[iT][alpha]); nPhFbar2 += Fbar2(bStart2,bStop2); //nPh + 1 - f2
+						for(int jDir=0; jDir<3; jDir++)
+						{	matrix dRhoAcomm_j = dRho1[jDir] * A[alpha] * nPhFbar2 - nPhF1 * A[alpha] * dRho2[jDir];
+							for(int iDir=0; iDir<3; iDir++)
+								contribGamma_dRho(iDir,jDir) = prefacGamma * dotc(SAcomm[alpha][iDir], dRhoAcomm_j).real();
+						}
+						Gamma[iMuT+1] += contribGamma_dRho;
+						if(valley)
+							GammaV[iMuT+1] += contribGamma_dRho * wValley;
+					}
 			}
 		}
 	}
@@ -200,11 +257,7 @@ struct SpinRelaxCollect
 		
 		//Select relevant band range:
 		int bStart = nBands, bStop = 0;
-		for(int b=0; b<nBands; b++)
-		{	const double& E = e.E[b];
-			if(E>=Estart and b<bStart) bStart=b;
-			if(E<=Estop and b>=bStop) bStop=b+1;
-		}
+		bandRangeNarrow(e, bStart, bStop);
 		int nBandsSel = bStop - bStart; //reduced number of selected bands at this k-pair
 		if(nBandsSel <= 0) return;
 		
@@ -226,13 +279,26 @@ struct SpinRelaxCollect
 		for(size_t iT=0; iT<T.size(); iT++)
 		{	double invT = 1./T[iT];
 			for(size_t iMu=0; iMu<dmu.size(); iMu++)
-			{	size_t iMuT = iT*dmu.size() + iMu; //combined index
-				//Compute Fermi occupations and accumulate chi contributions:
+			{	size_t iMuT = (iT*dmu.size() + iMu)*2; //combined index
+				//Compute Fermi occupations and accumulate chi contributions (FGR formula):
 				diagMatrix F(nBands), Fbar(nBands);
 				for(int b=bStart; b<bStop; b++)
 				{	fermi(invT*(e.E[b] - dmu[iMu]), F[b], Fbar[b]);
 					chi[iMuT] += (invT * F[b]*Fbar[b]) * contribChi[b];
 				}
+				//Accumulate chi contributions (dRho formula):
+				matrix3<> contribChi_dRho;
+				for (int jDir = 0; jDir < 3; jDir++)
+				{	matrix dRho_j = dRhoCalc(e.E, F, T[iT], e.S[jDir], bStart, bStop);
+					const complex* dRho_jData = dRho_j.data();
+					for(int b2 = bStart; b2 < bStop; b2++)
+					for(int b1 = bStart; b1 < bStop; b1++)
+					{	for(int iDir = 0; iDir<3; iDir++)
+							contribChi_dRho(iDir, jDir) += (e.S[iDir](b1, b2).conj() * (*dRho_jData)).real();
+						dRho_jData++;
+					}
+				}
+				chi[iMuT+1] += prefacChi * contribChi_dRho;
 			}
 		}
 	}
@@ -288,6 +354,7 @@ int main(int argc, char** argv)
 	InputMap inputMap(ip.inputFilename);
 	const int nOffsets = inputMap.get("nOffsets"); assert(nOffsets>0);
 	const int nBlocks = inputMap.get("nBlocks"); assert(nBlocks>0);
+	const double degeneracyThreshold = inputMap.get("degeneracyThreshold", 1e-6/eV) * eV;
 	const double EconserveWidth = inputMap.get("EconserveWidth") * eV;
 	const double Tmin = inputMap.get("Tmin") * Kelvin; //temperature; start of range
 	const double Tmax = inputMap.get("Tmax", Tmin/Kelvin) * Kelvin; assert(Tmax>=Tmin); //temperature; end of range (defaults to Tmin)
@@ -305,6 +372,7 @@ int main(int argc, char** argv)
 	logPrintf("Tmin = %lg\n", Tmin);
 	logPrintf("Tmax = %lg\n", Tmax);
 	logPrintf("Tcount = %lu\n", Tcount);
+	logPrintf("degeneracyThreshold = %lg\n", degeneracyThreshold);
 	logPrintf("EconserveWidth = %lg\n", EconserveWidth);
 	logPrintf("dmuMin = %lg\n", dmuMin);
 	logPrintf("dmuMax = %lg\n", dmuMax);
@@ -382,8 +450,8 @@ int main(int argc, char** argv)
 	std::vector<std::shared_ptr<SpinRelaxCollect>> srcArr(nBlocks);
 	for(int block=0; block<nBlocks; block++)
 	{	logPrintf("Working on block %d of %d: ", block+1, nBlocks); logFlush();
-		srcArr[block] = std::make_shared<SpinRelaxCollect>(dmu, T, omegaPhByTmin, nModes, EconserveWidth,
-			nKpairsPerBlock, nKtotPerBlock, fw.ePhEstart, fw.ePhEstop, valley, fw.R);
+		srcArr[block] = std::make_shared<SpinRelaxCollect>(dmu, T, omegaPhByTmin, nModes, degeneracyThreshold,
+			EconserveWidth, nKpairsPerBlock, nKtotPerBlock, fw.ePhEstart, fw.ePhEstop, valley, fw.R);
 		SpinRelaxCollect& src = *(srcArr[block]);
 		for(int o=0; o<noMine; o++)
 		{	Random::seed(block*nOffsetsPerBlock+o+oStart); //to make results independent of MPI division
@@ -405,10 +473,12 @@ int main(int argc, char** argv)
 	
 	//Report results with statistics:
 	const double ps = 1e3*fs; //picosecond
+	const char* formulaName[2] = { "FGR", "dRho" };
 	for(size_t iT=0; iT<Tcount; iT++)
 	for(size_t iMu=0; iMu<dmuCount; iMu++)
-	{	size_t iMuT = iT*dmuCount + iMu; //combined index
-		logPrintf("\nResults for T = %lg K and dmu = %lg eV:\n", T[iT]/Kelvin, dmu[iMu]/eV);
+	for(size_t iFormula=0; iFormula<2; iFormula++)
+	{	size_t iMuT = 2*(iT*dmuCount + iMu)+iFormula; //combined index
+		logPrintf("\nResults for T = %lg K and dmu = %lg eV using %s formula:\n", T[iT]/Kelvin, dmu[iMu]/eV, formulaName[iFormula]);
 		std::vector<matrix3<>> Gamma(nBlocks), chi(nBlocks), T1bar(nBlocks), T1Vbar(nBlocks);
 		std::vector<double> T1(nBlocks), T1V(nBlocks);
 		for(int block=0; block<nBlocks; block++)
@@ -425,13 +495,13 @@ int main(int argc, char** argv)
 				T1V[block] = (1./3)*trace(T1Vbar[block]);
 			}
 		}
-		reportResult(Gamma, "Gamma", 1./(eV*ps), "1/(eV.ps)");
-		reportResult(chi, "chi", 1./eV, "1/eV");
-		reportResult(T1bar, "T1", ps, "ps"); //tensor version
-		reportResult(T1, "T1", ps, "ps");
+		reportResult(Gamma, "Gamma", 1./(eV*ps), "1/(eV.ps)", globalLog, true);
+		reportResult(chi, "chi", 1./eV, "1/eV", globalLog, true);
+		reportResult(T1bar, "T1", ps, "ps", globalLog, true); //tensor version
+		reportResult(T1, "T1", ps, "ps", globalLog, true);
 		if(valley)
-		{	reportResult(T1Vbar, "T1valley", ps, "ps"); //tensor version
-			reportResult(T1V, "T1valley", ps, "ps");
+		{	reportResult(T1Vbar, "T1valley", ps, "ps", globalLog, true); //tensor version
+			reportResult(T1V, "T1valley", ps, "ps", globalLog, true);
 		}
 	}
 	
