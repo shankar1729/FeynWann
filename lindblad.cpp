4eeb80dc4411e3c96a0fbada518d8c55648c5e2b
Adela Habib <adelahanihabib@gmail.com>
2020-10-20 22:08:09 -0400

    Changed spin expectation values' printing precision

--- lindblad.cpp
+++ lindblad.cpp
@@ -583,7 +583,7 @@ struct Lindblad : public Integrator<DM1>
 		if(mpiWorld->isHead())
 		{	//Report step ID and energy:
 			logPrintf("Integrate: Step: %4d   t[fs]: %6.1lf   Etot[eV]: %.6lf   dfMax: %.4lf", stepID, t/fs, Etot/eV, dfMax);
-			if(spinorial) logPrintf("   S: [ %.4lg %.4lg %.4lg ]", Stot[0],  Stot[1],  Stot[2]);
+			if(spinorial) logPrintf("   S: [ %16.15lg %16.15lg %16.15lg ]", Stot[0],  Stot[1],  Stot[2]);
 			logPrintf("\n"); logFlush();
 			//Save distribution functions:
 			ofstream ofs("dist."+ossID.str());
