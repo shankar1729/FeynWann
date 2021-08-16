53461eef40592e0a8e5c428316436bee65e3987a
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2021-08-16 08:28:39 -0400

    Cleaned up plot script and added gitignore for spin_echo directory

--- lindblad/spin_echo/plotSpinTdep.py
+++ lindblad/spin_echo/plotSpinTdep.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 import numpy as np
 import matplotlib.pyplot as plt
 import glob
@@ -9,7 +8,6 @@ plt.rcParams['figure.dpi']      = 100
 plt.rcParams['image.cmap']      = 'jet'
 plt.rcParams['lines.linewidth'] = 2
 plt.rcParams['font.family']     = 'serif'
-plt.rcParams['font.weight']     = 'bold'
 plt.rcParams['font.size']       = 25
 plt.rcParams['font.sans-serif'] = 'serif'
 plt.rcParams['text.usetex']     = False
@@ -35,58 +33,37 @@ plt.rcParams['ytick.direction']  = 'in'
 
 omega = 41.3414 # taken from test.cpp
 
-plt.figure(1, figsize=(12,6))
 styles=['solid','solid', 'solid', 'solid','solid', 'solid', 'solid']
-SxArray = []; SyArray = []; SzArray = []
-SxPrimeArray = []; SyPrimeArray = []
-for iFile, fname in enumerate(glob.glob('xRelax*.out')):
-	print(iFile, fname); title = fname.replace('xRelax', '').replace('.out','')
+for iFile, fname in enumerate(glob.glob('*.out')):
+	fig, axes = plt.subplots(3, 1, figsize=(12, 9), sharex=True)
+	plt.subplots_adjust(hspace=0.1)
+	title = fname.replace('.out','')
+	print(f'Plotting {title}')
 	t = []
-	Sx = []; Sy = []; Sz = []
+	S = []
 	for line in open(fname):
 		if line.startswith('Integrate: Step:'):
 			tokens = line.split()
 			t.append(float(tokens[4]))
-			Sx.append(float(tokens[11])); Sy.append(float(tokens[12])); Sz.append(float(tokens[13]))
+			S.append([float(tok) for tok in tokens[11:14]])
 	tPS = np.array(t[1:])*0.001
-	Sx = np.array(Sx[1:])
-	Sy = np.array(Sy[1:])
-	Sz = np.array(Sz[1:])
-	SxArray.append(Sx)
-	SyArray.append(Sy)
-	SzArray.append(Sz)
+	S = np.array(S[1:]).T
 	
-	SxPrime =  Sx*np.cos(omega*tPS) + Sy*np.sin(omega*tPS)
-	SyPrime = -Sx*np.sin(omega*tPS) + Sy*np.cos(omega*tPS)
-	SxPrimeArray.append(SxPrime)
-	SyPrimeArray.append(SyPrime)
+	#Rotating frame versions:
+	Sprime = np.zeros(S.shape)
+	Sprime[0] =  S[0]*np.cos(omega*tPS) + S[1]*np.sin(omega*tPS)
+	Sprime[1] = -S[0]*np.sin(omega*tPS) + S[1]*np.cos(omega*tPS)
+	Sprime[2] = S[2]
 
-
-plt.plot(tPS, SxArray[0])
-plt.plot(tPS, SxPrimeArray[0])
-plt.axvline(100 + 3.7995, linestyle='--', color='black')
-plt.axvline(200 + 3.7995, linestyle='--', color='black')
-
-#plt.plot(tPS, np.sin(omega*tPS))
-#plt.plot(tPS, SxPrime)
-
-#for i in range(len(SxArray)):
-	#Sx = SxArray[i]
-	#Sy = SyArray[i]
-	#Sz = SzArray[i]
-
-	##plt.plot(tPS, Sz/Sz[0], ls=styles[iFile])
-	#plt.plot(tPS, Sx/Sz[0], label=title, ls=styles[iFile], alpha=1)
-	##plt.plot(tPS, Sy/Sz[0], label=title, ls=styles[iFile])
-	
-#plt.legend('$\omega = 0$')
-#plt.axhline(1, color='k', ls='dotted', lw=1)
-#plt.axhline(0, color='k', ls='dotted', lw=1)
-#plt.axhline(-1, color='k', ls='dotted', lw=1)
-plt.xlabel(r'$t$ [ps]'); plt.xlim(0, 100)
-#plt.ylabel(r'$\langle S_x(t) \rangle / \langle S_x(0) \rangle$')
-plt.ylabel(r'$\langle S_x(t) \rangle$')
-plt.xlim(xmax=tPS[-1])
-
-plt.savefig('/home/mani/tmp/S_x_with_echo.png', bbox_inches='tight')
+	#Plot each direction in separate panel:
+	for iDir, ax in enumerate(axes):
+		plt.sca(ax)
+		plt.plot(tPS, S[iDir])
+		plt.plot(tPS, Sprime[iDir])
+		plt.axvline(100 + 3.7995, linestyle='--', color='black')
+		plt.axvline(200 + 3.7995, linestyle='--', color='black')
+		plt.ylabel(fr'$\langle S_{"xyz"[iDir]}(t) \rangle$')
+	plt.xlabel(r'$t$ [ps]')
+	plt.xlim(xmax=tPS[-1])
+	plt.savefig(f'{title}.png', bbox_inches='tight')
 plt.show()
