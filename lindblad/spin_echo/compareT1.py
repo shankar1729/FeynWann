60d918498e8d8d984885a32d0bdb4f787911fa09
Ravishankar Sundararaman <shankar.progs2@gmail.com>
2023-03-30 08:11:22 -0400

    Added T1 test for model EY

---
+++ lindblad/spin_echo/compareT1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+ps = 1000. #in fs
+EhInv = 0.024188  #Eh^-1 in fs
+
+
+for iDir, dirName in enumerate("xyz"):
+    fname = f'T1{dirName}.out'
+    if not os.path.isfile(fname):
+        continue
+    t = []
+    Sdir = []
+    for line in open(fname):
+        if line.startswith('Integrate: Step:'):
+            tokens = line.split()
+            t.append(float(tokens[4]))
+            Sdir.append(float(tokens[11 + iDir]))
+    t = np.array(t[1:])
+    Sdir = np.array(Sdir[1:])
+    plt.plot(t/ps, Sdir, label=f"$S_{dirName}$")
+plt.ylabel(fr'$\langle S_{"xyz"[iDir]}(t) \rangle$')
+plt.xlabel(r'$t$ [ps]')
+plt.xlim(t[0]/ps, t[-1]/ps)
+plt.savefig(f'T1.pdf', bbox_inches='tight')
+plt.legend()
+plt.show()
